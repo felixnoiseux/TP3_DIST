2c3f20f17a43989a1f20bf57caa72280ac5484a2
Fantom7393 <raphael.lapointe99@gmail.com>
2020-12-15 22:46:10 -0500

    Créé questions

--- Erlang/exercice2.erl
+++ Erlang/exercice2.erl
@@ -1,5 +1,6 @@
 -module(exercice2).
 -include("expr.hrl").
+-include("question.hrl").
 -compile(export_all).
 
 % Lit le fichier, le ferme et retourne les lignes du fichier sous-forme de liste afin 
@@ -37,6 +38,7 @@ lignesVersExpressions(Liste) ->
         [Record] ++ lignesVersExpressions(lists:nthtail(2, Liste))
     end.
 
+% Retourne l'expression qui correspond à celle entrée par l'utilisateur afin que le main puisse écrire sa signification.
 trouverExpression([], Recherche) ->
     throw("Aucun match.");
 trouverExpression(Liste, Recherche) ->
@@ -48,10 +50,30 @@ trouverExpression(Liste, Recherche) ->
         trouverExpression(lists:nthtail(1, Liste), Recherche)
     end.
 
+obtenirPhraseInterrogative(Phrase) ->
+    "".
+
+genererQuestions() ->
+    [
+        #question{
+            question = "La vitesse anémométrique d'une hirondelle dépend de son continent d'origine (Europe ou Afrique) (Ceux qui ont vu Monty Python and The Holy Grail comprendront la référence).",
+            reponse = "V"
+        },
+        #question{question = "C# est un langage de programmation en tout point supérieur à Java.", reponse = "V"},
+        #question{question = "Google a fait le bon choix en imposant aux développeurs Android d'utiliser le ScopedStorage.", reponse = "F"},
+        #question{question = "Les prix des produits Apple sont toujours justifiés.", reponse = "F"},
+        #question{question = "Dans Star Wars, les Jedis ont le droit de se marier.", reponse = "F"},
+        #question{question = "Node.js est un bien meilleur outil de développement web qu'ASP.NET.", reponse = "V"},
+        #question{question = "Dans le Seigneur des Anneaux, Aragorn a l'air plutôt jeune, mais en réalité, il a 87 ans!!!", reponse = "V"},
+        #question{question = "En ce moment, vous pensez à une pomme.", reponse = "F"}
+    ].
+
 % Point d'entrée. Toute les autres fonctions sont appelées ici. Directement ou indirectement.
 main() ->
     Liste = lireFichier("expression.csv"),
     Expressions = lignesVersExpressions(Liste),
     Expr = lists:droplast(io:get_line("Entrez une expression: ")),
     Match = trouverExpression(Expressions, Expr),
-    io:fwrite(Match#expr.signification ++ "\n").
+    io:fwrite(Match#expr.signification ++ "\n"),
+    Phrase = obtenirPhraseInterrogative(lists:droplast(io:get_line("Entrez une phrase à convertir en phrase interrogative: "))),
+    io:fwrite(Phrase).
---
+++ Erlang/question.hrl
@@ -0,0 +1,4 @@
+-record(question, {
+    question = "La vitesse anémométrique d'une hirondelle dépend de son continent d'origine (Europe ou Afrique)(Ceux qui ont vu Monty Python and The Holy Grail comprendront la référence.)",
+    reponse = ""
+}).
